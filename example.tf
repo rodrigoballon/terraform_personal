1d03b0abcbbc0d77c5e22be1eb6d146cb49a49b5
Rodrigo Ballon <rballon@paypal.com>
2016-10-19 13:10:13 -0400

    Adding variables.tf

--- example.tf
+++ example.tf
@@ -1,13 +1,12 @@
 provider "aws" {
-  region                   = "us-east-1"
+  region                   = "${var.region}"
   shared_credentials_file  = "/Users/rballon/.aws/credentials_personal"
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-13be557e"
+  ami           = "${lookup(var.amis, var.region)}"
   instance_type = "t2.micro"
-
+}
   provisioner "local-exec" {
     command = "echo ${aws_instance.example.public_ip} > ip_address.txt"
-  }
 }
---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "amis" {
+  type = "map"
+  default = {
+    us-east-1 = "ami-13be557e"
+    us-west-2 = "ami-06b94666"
+  }
+}
