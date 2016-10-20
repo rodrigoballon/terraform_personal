77845a3e183ade4ce6d9046dc303ee3d53578fc4
Rodrigo Ballon <rballon@paypal.com>
2016-10-20 09:41:04 -0400

    Removing outpu

--- example.tf
+++ example.tf
@@ -7,6 +7,6 @@ resource "aws_instance" "example" {
   ami           = "${lookup(var.amis, var.region)}"
   instance_type = "t2.micro"
 }
-  provisioner "local-exec" {
-    command = "echo ${aws_instance.example.public_ip} > ip_address.txt"
+provisioner "local-exec" {
+  command = "echo ${aws_instance.example.public_ip} > ip_address.txt"
 }
