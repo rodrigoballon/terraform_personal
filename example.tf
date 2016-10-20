913fc8bae673a26a8fd98f58398024198d59f66b
Rodrigo Ballon <rballon@paypal.com>
2016-10-20 10:17:30 -0400

    Adding variables

--- example.tf
+++ example.tf
@@ -1,12 +1,17 @@
 provider "aws" {
   region                   = "${var.region}"
-  shared_credentials_file  = "/Users/rballon/.aws/credentials_personal"
+  shared_credentials_file  = "/etc/secrets/.aws/credentials_personal"
 }
 
 resource "aws_instance" "example" {
   ami           = "${lookup(var.amis, var.region)}"
   instance_type = "t2.micro"
+  provisioner "local-exec" {
+    command = "echo ${aws_instance.example.public_ip} > ip_address.txt"
+  }
 }
-provisioner "local-exec" {
-  command = "echo ${aws_instance.example.public_ip} > ip_address.txt"
+
+resource "aws_eip" "ip" {
+    instance = "${aws_instance.example.id}"
+    depends_on = ["aws_instance.example"]
 }
