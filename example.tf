7a1ed55872d6a4c43bd815f2c1614a91724bba59
Rodrigo Ballon <rballon@paypal.com>
2016-10-20 10:20:39 -0400

    Fixing indentation

--- example.tf
+++ example.tf
@@ -1,14 +1,14 @@
 provider "aws" {
-  region                   = "${var.region}"
-  shared_credentials_file  = "/etc/secrets/.aws/credentials_personal"
+    region                   = "${var.region}"
+    shared_credentials_file  = "/etc/secrets/.aws/credentials_personal"
 }
 
 resource "aws_instance" "example" {
-  ami           = "${lookup(var.amis, var.region)}"
-  instance_type = "t2.micro"
-  provisioner "local-exec" {
-    command = "echo ${aws_instance.example.public_ip} > ip_address.txt"
-  }
+    ami           = "${lookup(var.amis, var.region)}"
+    instance_type = "t2.micro"
+    provisioner "local-exec" {
+        command = "echo ${aws_instance.example.public_ip} > ip_address.txt"
+    }
 }
 
 resource "aws_eip" "ip" {
